4cf41a6df4d4299917325b8ab1fed33a6f496ab3
Anthony Cantu <anthony10293@gmail.com>
2025-01-22 16:24:11 -0600

    Added greetings script

---
+++ linux_practice/greetings.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This script greets the user with a personalized message.
+
+# Ask for the user's name
+echo "What is your name?"
+read name
+
+# Display a personalized greeting
+echo "Hello, $name! Hope you are doing well!"
